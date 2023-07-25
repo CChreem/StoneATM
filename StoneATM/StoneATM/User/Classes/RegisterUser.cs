dab050456b521bc4a57ac427e5d5c91d7c4edf9d
CChreem <81775781+CChreem@users.noreply.github.com>
2023-07-25 13:14:28 -0300

    Add files via upload

---
+++ StoneATM/StoneATM/User/Classes/RegisterUser.cs
@@ -0,0 +1,22 @@
+﻿using StoneATM.User.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneATM.User.Classes
+{
+    public class RegisterUser : IRegisterUser
+    {
+        public void CreatePassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StoneATM/StoneATM/User/Interfaces/IRegisterUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneATM.User.Interfaces
+{
+    public interface IRegisterUser
+    {
+        void Register();
+        void CreatePassword();
+    }
+}
